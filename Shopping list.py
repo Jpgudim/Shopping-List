ef4865ee9bc3385bcc22ce493912d049fe2f9ad3
Jordan Gudim <137233322+Jpgudim@users.noreply.github.com>
2023-06-22 22:45:52 -0500

    Add files via upload

---
+++ Shopping list.py
@@ -0,0 +1,8 @@
+#This is a practice file for lists
+
+shopping_list = ["eggs", "milk"]
+response = input("You need eggs and milk, what else do you need to buy? ")
+shopping_list.append(response)
+
+print ("Your shoppping list:")
+print (shopping_list)
